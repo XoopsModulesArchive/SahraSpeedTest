f814f39ae91532f19a06829b0fc9116a0a692f7a
mambax7 <mambax7@gmail.com>
2020-10-25 03:38:42 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include '../../mainfile.php';
+include '../../header.php';
+include 'index.htm';
+include 'header.php';
+include '../../footer.php';
---
+++ xoops_version.php
@@ -0,0 +1,25 @@
+<?php
+
+/*********************************************************/
+/* Sahra Ýnternet ve Biliþim Hizmetleri Xoops Destek Bölümü                      */
+/* Sahra Tarafýndan Yapýlmýþtýr                                         */
+/* Not:Ücretsiz Bir Modüldür.Ýstediðiniz Gibi Geliþtirebilirsiniz                                    */
+/* http://www.xoopsum.com                            */
+/*                            */
+/*                          */
+/*********************************************************/
+
+/**
+ *  Ýnternet Hýz Testi Modülü/Internet Speed Test
+ *  Xoopsum.Com | Sahra Ýnternet Hizmetleri Genel Yönetici Sahra Tarafýndan Oluþturulmuþtur
+ */
+$modversion['name'] = 'SahraSpeedTest';
+$modversion['dirname'] = 'SahraSpeedTest';
+$modversion['hasMain'] = '1';
+$modversion['description'] = 'Ýnternet Hýz Testi/Internet Speed Test';
+$modversion['version'] = '1.1';
+$modversion['author'] = 'Xoopsum.Com|Sahra Ýnternet Hizmetleri';
+$modversion['credits'] = 'Sahra';
+$modversion['license'] = 'GPL';
+$modversion['official'] = 'No';
+$modversion['image'] = 'images/logo.gif';
